7e991c54a2b87f02817ecc31772053e0fc4edb74
IshaanJolly <ijolly1997@gmail.com>
2025-03-17 10:42:08 +0000

    feat test files added to check

---
+++ src/test.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+def greet( name: str ):
+
+
+
+
+    print( " Hello, "  +  name )  
+
+def main():
+    
+    
+    
+    
+  greet("Alice")
+  greet("Bob")
+
+if __name__ == "__main__":
+    
+    
+      main()

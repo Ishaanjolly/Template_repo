04f4f8e87c53ae37fbd8fe00b4bcedaf66bfaa4a
IshaanJolly <ijolly1997@gmail.com>
2025-03-17 13:47:52 +0000

    feat test pre-commit

--- src/test.py
+++ src/test.py
@@ -1,29 +1,11 @@
-import os
-import sys
+def greet(name: str):
+    print(" Hello, " + name)
 
-def greet( name: str ):
-
-
-
-
-
-
-
-
-
-    print( " Hello, "  +  name )  
 
 def main():
-    
-    
-    
-    
-    
+    greet("Alice")
+    greet("Bob")
 
-  greet("Alice")
-  greet("Bob")
 
 if __name__ == "__main__":
-    
-    
-      main()
+    main()
